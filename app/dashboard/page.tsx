39ca50d9119a5f6b50eea688eb3837099aeb7d81
Rifkhan Abdul Rahman <ra37@staff.staffs.ac.uk>
2025-02-04 15:46:13 +0000

    chapter 10

--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <p>Dashboard Test Page</p>;
-  }
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,28 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p> */}
+                <Image
+            src="/app-logo.jpg"
+            width={300}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+                    <Image
+            src="/app-logo.jpg"
+            width={80}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
     </div>
   );
 }
